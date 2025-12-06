e92cb5840b4d7c3d7bcfd82fdeb771fa8b16bcac
Maksymilian Cieślak <mc123.pl@gmail.com>
2025-12-06 18:56:23 +0100

    Properties, Encje User i Book (Dev1 main)

---
+++ src/main/java/com/library/management/model/Book.java
@@ -0,0 +1,22 @@
+package com.library.management.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "books")
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column
+    private boolean available = true;
+}
